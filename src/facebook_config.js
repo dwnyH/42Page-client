8361321cc83b0e5293741f41adefcb7499bf9b57
dawoon <dawoon316@naver.com>
2019-04-22 22:13:37 +0900

    Add environment variables

---
+++ src/facebook_config.js
@@ -0,0 +1,10 @@
+const config = {
+  apiKey: process.env.FACEBOOK_API_KEY,
+  authDomain: process.env.FACEBOOK_AUTH_DOMAIN,
+  databaseURL: process.env.FACEBOOK_DATABASE_URL,
+  projectId: process.env.FACEBOOK_PROJECT_ID,
+  storageBucket: process.env.FACEBOOK_STORAGE_BUCKET,
+  messagingSenderId: process.env.FACEBOOK_MESSAGING_SENDER_ID,
+};
+
+export default config;
