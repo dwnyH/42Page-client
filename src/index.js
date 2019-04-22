ddcce2f0e596daa347492f5bfe5016a9dff68251
dawoon <dawoon316@naver.com>
2019-04-22 21:44:37 +0900

    Remove unused code

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import rootReducer from './reducers/index';
 import './index.scss';
 import App from './containers/App';
 import ScrollToTop from './components/ScrollToTop/ScrollToTop';
-import * as serviceWorker from './serviceWorker';
 
 const store = createStore(rootReducer, applyMiddleware(logger));
 
@@ -22,8 +21,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root'),
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
