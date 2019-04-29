970e6d4b1bc583a1d1a3fb9dca6dad08ca715f8c
dawoon <dawoon316@naver.com>
2019-04-29 16:15:15 +0900

    Modify environment variables

--- src/facebook_config.js
+++ src/facebook_config.js
@@ -1,10 +1,10 @@
 const config = {
-  apiKey: process.env.FACEBOOK_API_KEY,
-  authDomain: process.env.FACEBOOK_AUTH_DOMAIN,
-  databaseURL: process.env.FACEBOOK_DATABASE_URL,
-  projectId: process.env.FACEBOOK_PROJECT_ID,
-  storageBucket: process.env.FACEBOOK_STORAGE_BUCKET,
-  messagingSenderId: process.env.FACEBOOK_MESSAGING_SENDER_ID,
+  apiKey: process.env.REACT_APP_FACEBOOK_API_KEY,
+  authDomain: process.env.REACT_APP_FACEBOOK_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FACEBOOK_DATABASE_URL,
+  projectId: process.env.REACT_APP_FACEBOOK_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FACEBOOK_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FACEBOOK_MESSAGING_SENDER_ID,
 };
 
 export default config;
